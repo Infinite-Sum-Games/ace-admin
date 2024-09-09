6c62a6885cbe3d9bc4df8d06fe9c5fdee2b58410
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2024-09-10 02:03:00 +0530

    Init: React, TS, Shadcn, Recoil, Tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,12 @@
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
-// import devtools from 'solid-devtools/vite';
+import react from '@vitejs/plugin-react-swc'
+import path from "path"
+import { defineConfig } from "vite"
 
 export default defineConfig({
-  plugins: [
-    /* 
-    Uncomment the following line to enable solid-devtools.
-    For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
-    */
-    // devtools(),
-    solidPlugin(),
-  ],
-  server: {
-    port: 3000,
-  },
-  build: {
-    target: 'esnext',
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
